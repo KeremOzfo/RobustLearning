b06e35d63f05bf2313d30ea1d1d9cbf945f8ec76
Emre Ozfatura <48322751+emre1925@users.noreply.github.com>
2021-02-23 13:37:31 +0000

    Update dataset_utlis.py

--- dataset_utlis.py
+++ dataset_utlis.py
@@ -3,7 +3,7 @@ import torch
 def similarity_loss(x,bs):
     loss=torch.zeros(1,bs)
     for i in range(bs):
-        loss[i]=torch.sum(x[i][:]*x[i+bs][:])/(torch.norm(x[i][:])*torch.norm(x[i+bs][:])) # Cosine similarity
+        loss[0][i]=torch.sum(x[i][:]*x[i+bs][:])/(torch.norm(x[i][:])*torch.norm(x[i+bs][:])) # Cosine similarity
     loss=loss.mean()    
     return loss
 
