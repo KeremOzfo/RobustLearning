b6e5570f111965a223b20e61cd5c05a74ce407c0
Emre Ozfatura <48322751+emre1925@users.noreply.github.com>
2021-02-23 12:19:24 +0000

    Update dataset_utlis.py

--- dataset_utlis.py
+++ dataset_utlis.py
@@ -1,5 +1,13 @@
 import torch
 
+def similarity_loss(x,bs):
+    loss=torch.zeros(1,bs)
+    for i in range(bs):
+        loss[i]=torch.sum(x[i][:]*x[i+bs][:])/(torch.norm(x[i][:])*torch.norm(x[i+bs][:])) # Cosine similarity
+    loss=loss.mean()    
+    return loss
+
+
 def gaussian_noise(batch,mean=0,std=1):
     new_batch = batch.add(torch.randn(batch.size()).to(batch) * std + mean)
     return new_batch
